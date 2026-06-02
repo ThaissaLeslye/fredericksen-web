81d6291f48655cd7b4f35729df829a0c79e991fd
thaissa <thaissa.leslye.tl@gmail.com>
2026-06-02 21:46:17 +0000

    fix: remove devtools that was generating erros in tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     vue(),
-    vueDevTools({
-      appendTo: 'src/main.ts',
-    }),
   ],
   resolve: {
     alias: {
